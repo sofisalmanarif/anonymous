1616718b387963432842fad26333c7b9a6afcee1
sofisalmanarif <sofisalman9906@gmail.com>
2024-09-18 22:28:45 +0530

    database models done

---
+++ src/models/user.ts
@@ -0,0 +1,77 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface Message extends Document {
+    content: string,
+    createAt: Date,
+}
+
+export interface User extends Document {
+    username: string,
+    email: string,
+    password: string,
+    messages: Message[],
+    verificationCode :string,
+    isVerified: boolean,
+    verificationCodeExpiry: Date,
+    isAcceptionMessages :boolean,
+
+
+
+}
+
+const messageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const userSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique:true,
+        trim:true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    verificationCode: {
+        type: String,
+        required: true,
+
+    },
+    verificationCodeExpiry: {
+        type: Date,
+        required: true,
+
+    },
+    isAcceptionMessages: {
+        type: Boolean,
+        required: true,
+        default:true
+
+    },
+    messages:[messageSchema]
+
+})
+
+
+const userModel = mongoose.models.User as mongoose.Model<User> || mongoose.model<User>("User",userSchema)
+const messageModel = mongoose.models.User as mongoose.Model<User> || mongoose.model<User>("User",userSchema)
+export {userModel,messageModel}  ;  //export the model to use it in other files
+// export default messageModel
+
+// export default userModel

eac5aa7fbcdb05630c1265cde77c751c369a6b9e
sofisalmanarif <sofisalman9906@gmail.com>
2024-09-21 00:17:53 +0530

    call backs added in next auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -40,6 +40,28 @@ export const options: NextAuthOptions = {
             }
         })
     ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if(user){
+                token._id = user._id?.toString();
+                token.username = user.username;
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessages = user.isAcceptingMessages
+            }
+            
+            return token
+          },
+        async session({ session,  token }) {
+            if(token){
+                session.user._id = token._id;
+                session.user.username = token.username;
+                session.user.isVerified = token.isVerified;
+                session.user.isAcceptingMessages = token.isAcceptingMessages 
+            }
+            return session
+          }
+         
+    },
     pages:{
         signIn: '/sign-in',
     },

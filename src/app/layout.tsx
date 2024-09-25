a47f0f2d3d965f674964ad28a763e32812c3b80d
sofisalmanarif <sofisalman9906@gmail.com>
2024-09-25 21:39:48 +0530

    bugs fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${geistMono.variable} antialiased`}
       >
         {children}
         <Toaster />

a8ea7f45430045e39957cbb4f86af412cf0f0d9f
sofisalmanarif <sofisalman9906@gmail.com>
2024-09-26 21:03:26 +0530

    navbar done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import { User } from 'next-auth'
+import { DividerHorizontalIcon } from '@radix-ui/react-icons'
+
+const Navbar = () => {
+    const {data,status} = useSession()
+    console.log(data,status)
+    const user: User = data?.user as User
+  return (
+    <nav className='flex items-center px-20 justify-between py-3 border-b-2 border-zinc-700'>
+    <Link href={"/"} className='text-2xl '>Anonymous</Link>
+    {
+        data ?
+        <div className='flex  items-center gap-5'>
+            <p>{data.user.username}</p>
+            <Button variant={'secondary'}>Log Out</Button>
+        </div> :
+        <Button>log In</Button>
+    }
+    
+    </nav>
+  )
+}
+
+export default Navbar
